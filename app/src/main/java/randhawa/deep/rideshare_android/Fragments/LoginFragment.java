3b4c735e2b653972f85a37e1a857820be4dddf46
Deep Randhawa <deepsaranr@gmail.com>
2015-06-30 09:47:10 -0400

    Successfully added Facebook Login

---
+++ app/src/main/java/randhawa/deep/rideshare_android/Fragments/LoginFragment.java
@@ -0,0 +1,91 @@
+package randhawa.deep.rideshare_android.Fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import randhawa.deep.rideshare_android.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+
+    private static LoginButton loginButton;
+    public static CallbackManager callbackManager;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.v("FB Login", "Successful Login to Facebook");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.v("FB Login", "Cancelled Login to Facebook");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.v("FB Login", "Error in Login to Facebook");
+            }
+        });
+        return view;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
--- app/src/main/java/randhawa/deep/rideshare_android/MainActivity.java
+++ app/src/main/java/randhawa/deep/rideshare_android/MainActivity.java
@@ -1,17 +1,33 @@
 package randhawa.deep.rideshare_android;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 
-public class MainActivity extends ActionBarActivity {
+import randhawa.deep.rideshare_android.Fragments.LoginFragment;
+
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // SETTING UP FACEBOOK LOGIN //
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        // SETTING UP LOGIN FRAGMENT //
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        Fragment loginFragment = new LoginFragment();
+        fragmentTransaction.add(R.id.login_fragment_view_group, loginFragment);
+        fragmentTransaction.commit();
     }
 
     @Override
